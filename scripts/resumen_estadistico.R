8d50aa581017c9f6b58751ed8c021085b7d13065
Mauro <mcejasmarcovecchio@gmail.com>
2026-02-21 18:40:07 -0300

    Graficos, Tabla, adaptacion a PDF y otros

---
+++ scripts/resumen_estadistico.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(gt)
+library(chromote)
+
+resumen_final <- read.csv("output/resumen_estadistico.csv")
+
+tabla_gt <- resumen_final |>
+  gt() |>
+  cols_label(X = "Variables") |>
+  fmt_number(decimals = 2) |>
+  
+  tab_options(
+    table.font.size = px(14),
+    table.font.names = "Serif",
+    column_labels.font.weight = "bold",
+    table.border.top.color = "black",
+    table.border.top.width = px(2),
+    table.border.bottom.color = "black",
+    table.border.bottom.width = px(2),
+    column_labels.border.bottom.color = "black",
+    column_labels.border.bottom.width = px(1),
+    data_row.padding = px(5),           # Espaciado para que no se vea apretada
+    table.width = px(600)               # Ancho fijo para consistencia
+  ) |>
+  
+  cols_align(align = "left", columns = 1) |>
+  cols_align(align = "center", columns = -1) 
+
+gtsave(tabla_gt, filename = "output/resumen_estad.png",
+       vwidth = 1500,
+       vheight = 1000,
+       zoom = 3)
